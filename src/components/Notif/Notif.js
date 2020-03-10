b304143933ab3cfdd801ac84cec2fd63bf5f46e7
dytra <widhi.nugraha@kbru.co.id>
2020-03-10 14:57:53 +0700

    add slide animation for notif panel

--- src/components/Notif/Notif.js
+++ src/components/Notif/Notif.js
@@ -4,7 +4,7 @@ import "./Notif.scss";
 const Notif = ({showNotif,toggleShowNotif}) => {
   
   return (
-    <div id="notif" style={{display: showNotif ? 'block' : 'none'}}>
+    <div id="notif" /*style={{display: showNotif ? 'block' : 'none'}}*/ className={!showNotif ? 'slideup' : ''}>
       <div id="notif-content">
         <div>By accessing and using this website, you acknowledge that you have read and
 understand our <a href="#">Cookie Policy</a>, <a href="#">Privacy Policy</a>, and our <a href="#">Terms of Service.</a></div>
